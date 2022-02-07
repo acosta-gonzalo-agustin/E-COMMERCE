f4ebaadb73ed687c6f8e1dc34e084fe7ad7a0fd1
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2022-02-07 12:16:03 -0300

    actualizando link

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, './public')));
 
  
 
-app.get('/home',function(req,res) {
+app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname,'views/index.html'));
 });
 
