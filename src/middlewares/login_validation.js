d46d970d72c6852724dac10cf4f419f1c427350a
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2022-03-23 12:41:10 -0300

    validacion login

--- src/middlewares/login_validation.js
+++ src/middlewares/login_validation.js
@@ -7,22 +7,26 @@ let users = JSON.parse(usersJSON);
 
 
 function verificacion(req,res,next) {
+    let condicion = 0;
     for(i of users) {
         if(req.body.email == i.email) {
+            condicion = 1;
             if(bcrypt.compareSync(req.body.password,i.contraseña)) {
-                break; 
-            } else {
-                console.log('llego');
-                res.render('/',{mensaje:'La contraseña que ingresaste es incorrecta'});
-            }
-        } else {
-            console.log('llego');
-            res.render('/',{mensaje:'El correo electrónico que ingresaste no está conectado a una cuenta. Encuentra tu cuenta e inicia sesión'});
+                req.session.user = [i.nombre,i.apellido,i.email,i.imagen];
+                condicion = 2;
+                break;
+            } 
         }
     }
-    
-    req.session.usuario  = req.body.email;
-    next();
+
+    if(condicion == 0) {
+        res.render('index',{mensaje:'El correo electrónico que ingresaste no está conectado a una cuenta. Encuentra tu cuenta e inicia sesión'});
+
+    } else if(condicion == 1) {
+        res.render('index',{old:req.body.email,mensaje:'La contraseña que ingresaste es incorrecta'});
+    } else {
+        next();
+    }
 }
 
 module.exports = verificacion;
