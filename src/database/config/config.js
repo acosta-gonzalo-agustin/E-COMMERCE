0d7206b29e0e18a92e1291aa8715174503a07b8a
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2022-04-15 12:30:59 -0300

    prueba_back

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -9,19 +9,17 @@ module.exports =
     'port':3306
   },
   "test": {
-    "username": "264166",
-    "password": 'LuisGonzalobsascba5',
-    "database": "gonzaloacosta_fastwheel",
-    "host": "mysql-gonzaloacosta.alwaysdata.net",
-    "dialect": "mysql",
-    'port':3306
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   "production": {
-    "username": "264166",
-    "password": 'LuisGonzalobsascba5',
-    "database": "gonzaloacosta_fastwheel",
-    "host": "mysql-gonzaloacosta.alwaysdata.net",
-    "dialect": "mysql",
-    'port':3306
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
 }
