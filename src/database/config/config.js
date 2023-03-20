a427518b853ec4d816afd33d92653ca1411ac4e6
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2023-03-20 20:07:44 -0300

    s

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,26 +1,28 @@
+require('dotenv').config();
+
 module.exports =
 {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "gonzaloacosta_fastwheel",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    'port':3306
+    "username": process.env.usernamed,
+    "password": process.env.passwordd,
+    "database": process.env.databased,
+    "host":     process.env.hostd,
+    "dialect":  process.env.dialectd,
+    'port':     process.env.portd
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.usernamet,
+    "password": process.env.passwordt,
+    "database": process.env.databaset,
+    "host"    : process.env.hostt,
+    "dialect" : process.env.dialectt
   },
   "production": {
-    "username": "264166",
-    "password": 'LuisGonzalobsascba5',
-    "database": "gonzaloacosta_fastwheel",
-    "host": "mysql-gonzaloacosta.alwaysdata.net",
-    "dialect": "mysql",
-    'port':3306
+    "username": process.env.usernamep,
+    "password": process.env.passwordp,
+    "database": process.env.databasep,
+    "host":     process.env.hostp,
+    "dialect": process.env.dialectp,
+    'port':    process.env.portp
   }
 }
