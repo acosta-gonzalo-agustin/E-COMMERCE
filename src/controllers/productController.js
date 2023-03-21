6bb207fa97f519318efd91bf13cc372918ddc2da
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2023-03-20 23:39:54 -0300

    s

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -624,7 +624,7 @@ const controlador = {
                         } else {
 
 
-                            const cloudy_data = cloudinary.uploader.upload(ruta, {public_id: nombre})
+                            const cloudy_data = cloudinary.uploader.upload(ruta, {folder: 'img-autos',public_id: nombre})
 
                             cloudy_data.then((data) => {
                                 console.log(data);
